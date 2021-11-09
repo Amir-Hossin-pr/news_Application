336ddc4b238a4da6725e20bb98d717d463d5245c
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-09 20:51:50 +0330

    Create account.service.js

---
+++ src/service/user/account.service.js
@@ -0,0 +1,22 @@
+async function login(user) {}
+
+async function signup(user) {}
+
+async function logout(req) {}
+
+async function signup(user) {}
+
+async function activateion(user) {}
+
+async function changePassword(user) {}
+
+async function forgotPassword(user) {}
+
+module.exports = {
+  login,
+  signup,
+  changePassword,
+  activateion,
+  forgotPassword,
+  logout,
+};
