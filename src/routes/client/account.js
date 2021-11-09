2f17d26a1149063b758dd8c916a334e6a6b3b7b5
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-09 21:13:50 +0330

    Update account.js

--- src/routes/client/account.js
+++ src/routes/client/account.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const { login } = require("../../service/user/account.service");
 
-router.post("/login", (req, res) => {});
+router.post("/login", async (req, res) => {
+  let user = {
+    userName: req.body.userName,
+    password: req.body.password,
+  };
+
+  let loginRes = await login(user);
+  res.json(loginRes);
+});
 
 router.post("/signup", (req, res) => {});
 
