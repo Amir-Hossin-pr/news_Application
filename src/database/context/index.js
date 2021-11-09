38a5d11163c1a8c0a2fab4a1347ac2968efb5459
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-09 19:15:26 +0330

    add data base models to model builder

--- src/database/context/index.js
+++ src/database/context/index.js
@@ -1,10 +1,25 @@
 const { Sequelize } = require("sequelize");
+const { applyExtraSetup } = require("./extraSetup");
 
 const sequelize = new Sequelize("News_DB", "amir", "1G14ijWA", {
   host: "localhost",
   dialect: "mssql",
   benchmark: true,
 });
+
+const modelDefiners = [
+  require("../models/news/news.model"),
+  require("../models/news/group.model"),
+  require("../models/users/role.model"),
+  require("../models/users/session.model"),
+  require("../models/users/user.model"),
+];
+
+for (const modelDefiner of modelDefiners) {
+  modelDefiner(sequelize);
+}
+
+applyExtraSetup(sequelize);
 
 sequelize.sync({ force: true });
 
