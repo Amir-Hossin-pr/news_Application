3d4273385a5da9ec56ffb1d4c13c298bccc063ae
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-13 21:58:48 +0330

    Create account.ts

---
+++ News/News.Server/routes/account/account.ts
@@ -0,0 +1,45 @@
+import * as express from 'express'
+import { messages } from '../../consts';
+const router = express.Router();
+import { AccountService } from "../../services/user/account.service";
+
+const accountService = new AccountService();
+
+router.post("/login", async (req: express.Request, res: express.Response) => {
+    let user = {
+        userName: req.body.userName,
+        password: req.body.password
+    }
+
+    let login = await accountService.login(user);
+    res.json(login);
+})
+
+router.post("/signup", async (req: express.Request, res: express.Response) => {
+
+})
+
+router.post("/forgotPassword", async (req: express.Request, res: express.Response) => {
+
+})
+
+router.post("/changePassword", async (req: express.Request, res: express.Response) => {
+
+})
+
+router.post("/activation", async (req: express.Request, res: express.Response) => {
+
+})
+
+router.get("/logout", async (req: express.Request, res: express.Response) => {
+    return accountService.logout(req.headers) ?
+        res.json({
+            status: true,
+            code: 200,
+            title: 'Success',
+            message: 'Logout Successfully'
+        }) :
+        res.json(messages.exception)
+})
+
+export default router;
