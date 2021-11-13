b773f8a0132e3393255859bb085456918f5f4c6e
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-13 21:58:22 +0330

    add user profile image item

--- News/News.Server/dataBase/models/users/user.model.ts
+++ News/News.Server/dataBase/models/users/user.model.ts
@@ -1,46 +1,51 @@
 import { DataTypes } from "sequelize";
+import { DataType } from "sequelize-typescript";
 
 module.exports = (sequelize) => {
-  sequelize.define("User", {
-    id:{
-      type:DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull:false,
-    },
-    fullName:{
-      type:DataTypes.STRING,
-      allowNull:false,
-      maxLength:250
-    },
-    userName:{
-      type:DataTypes.STRING,
-      allowNull:false,
-      maxLength:250
-    },
-    mobileNo:{
-      type:DataTypes.STRING,
-      allowNull:false,
-      maxLength:12,
-      minLength:10
-    },
-    password:{
-      type:DataTypes.STRING,
-      allowNull:false,
-    },
-    activeCode:{
-      type:DataTypes.STRING,
-      maxLength:10,
-      minLength:5,
-      allowNull:false,
-    },
-    isActive:{
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    registerDate:{
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-  });
+    sequelize.define("User", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        fullName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            maxLength: 250
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            maxLength: 250
+        },
+        mobileNo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            maxLength: 12,
+            minLength: 10
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        activeCode: {
+            type: DataTypes.STRING,
+            maxLength: 10,
+            minLength: 5,
+            allowNull: false,
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        image: {
+            type: DataType.STRING,
+            allowNull: true
+        },
+        registerDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    });
 };
