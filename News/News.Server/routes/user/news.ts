aafbf2e9ed7dba3dc1e676006571053015fc1104
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-13 16:55:50 +0330

    Update news.ts

--- News/News.Server/routes/user/news.ts
+++ News/News.Server/routes/user/news.ts
@@ -1,12 +1,15 @@
 
 import * as express from 'express'
 const router = express.Router();
-import NewsServices from "../../services/news/news.service"
+import { NewsServices } from "../../services/news/news.service"
 
-const newsServices: NewsServices = new NewsServices();
+const newsServices = new NewsServices();
 
 router.get("/:page/:count", async (req: express.Request, res: express.Response) => {
-    let news = await newsServices.getClientNews(parseInt(req.params.page), parseInt(req.params.count));
+    let news = await newsServices.getClientNews({
+        page: parseInt(req.params.page),
+        count: parseInt(req.params.count)
+    });
     res.json(news);
 })
 
