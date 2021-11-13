92c8597cec5295562c917ad52b9ab960877d9951
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-13 16:37:46 +0330

    Update news.admin.ts

--- News/News.Server/routes/admin/news.admin.ts
+++ News/News.Server/routes/admin/news.admin.ts
@@ -2,13 +2,21 @@ import * as express from 'express';
 const route = express.Router();
 import { messages } from "../../consts/index"
 import { AccountService } from "../../services/user/account.service";
+import { NewsServices } from "../../services/news/news.service";
 
 const accountServices = new AccountService();
+const newsServices = new NewsServices();
 
 route.use(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     let user = await accountServices.getUser(req.headers);
     if (user != null) {
-        next();
+        if (user.RoleId == 1) {
+            next();
+        }
+        else {
+            res.json(messages.accessDenied);
+            res.end();
+        }
     }
     else {
         res.json(messages.accessDenied);
@@ -16,8 +24,13 @@ route.use(async (req: express.Request, res: express.Response, next: express.Next
     }
 })
 
-route.get("/", (req: express.Request, res: express.Response) => {
-    res.json({ Admin: true })
+route.get("/:page/:count", async (req: express.Request, res: express.Response) => {
+    let news = await newsServices.getAdminNews({
+        page: parseInt(req.params.page),
+        count: parseInt(req.params.count)
+    })
+    res.json(news);
+    res.end();
 })
 
 export default route;
