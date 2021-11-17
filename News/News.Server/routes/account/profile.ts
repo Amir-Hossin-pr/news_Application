5c77a7d28754143aadd16e59dc652a56645fc9a2
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-17 22:01:08 +0330

    Update profile.ts

--- News/News.Server/routes/account/profile.ts
+++ News/News.Server/routes/account/profile.ts
@@ -2,16 +2,21 @@ import * as express from 'express'
 const router = express.Router();
 import { ProfileService } from "../../services/user/profile.service"
 
-const pofileService = new ProfileService()
+const profileService = new ProfileService()
 
 router.get("/", async (req: express.Request, res: express.Response) => {
-    let profile = await pofileService.getProfile(req.headers)
+    let profile = await profileService.getProfile(req.headers)
     res.json(profile)
     res.end()
 })
 
 router.post("/", async (req: express.Request, res: express.Response) => {
-
+    let body = req.body;
+    res.json(await profileService.updateProfile(req.headers, {
+        base64: body.base64,
+        fullName: body.fullName
+    }))
+    res.end()
 })
 
 export default router;
