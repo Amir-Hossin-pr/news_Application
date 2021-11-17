97756649c96ac1a81e28f02ba1af735fd7b77f80
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-17 23:18:58 +0330

    update panel

--- News/news.admin.panel/src/App.tsx
+++ News/news.admin.panel/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button'
 
 function App() {
     return (
         <div className="App">
-            <button>Test</button>
+            <Button variant="contained">contained</Button>
         </div>
     );
 }
