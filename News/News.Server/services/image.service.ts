5d23d9bd1449c88cc6ba567e8459fe98a4e2efd8
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-17 21:59:26 +0330

    check null or empty base64

--- News/News.Server/services/image.service.ts
+++ News/News.Server/services/image.service.ts
@@ -2,17 +2,20 @@ import * as crypto from 'crypto'
 import * as fs from 'fs'
 
 export async function saveImage(image: SaveImage) {
-    try {
-        const type = image.base64.split(';')[0].split('/')[1];
-        console.log(type);
-        let imageName = crypto.randomUUID() + Date.now() + `.${type}`;
-        let path = `public/images/${image.path}/` + imageName;
-        const base64Data = image.base64.replace(/^data:([A-Za-z+/]+);base64,/, '');
-        fs.writeFileSync(path, base64Data, { encoding: "base64" })
-        return imageName;
-    } catch {
-        return "";
+    if (image.base64 != null && image.base64 != "") {
+        try {
+            const type = image.base64.split(';')[0].split('/')[1];
+            console.log(type);
+            let imageName = crypto.randomUUID() + Date.now() + `.${type}`;
+            let path = `public/images/${image.path}/` + imageName;
+            const base64Data = image.base64.replace(/^data:([A-Za-z+/]+);base64,/, '');
+            fs.writeFileSync(path, base64Data, { encoding: "base64" })
+            return imageName;
+        } catch {
+            return "null.png";
+        }
     }
+    return "null.png";
 }
 
 type SaveImage = {
