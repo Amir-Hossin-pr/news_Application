5c52e51931a8001178f7e6099ddf358355d74d78
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-19 15:07:55 +0330

    delete all api calls

--- News/News.User.Panel/src/api/ApiCall.ts
+++ News/News.User.Panel/src/api/ApiCall.ts
@@ -2,22 +2,22 @@ import Axios from "axios";
 import Account from "@/services/AccountServices";
 
 const _config = {
-  baseURL: "https://localhost:5001/api/",
-  timeout: 60 * 1000,
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: Account.authenticationToken(),
-  },
+    baseURL: "https://localhost:3000/api/",
+    timeout: 60 * 1000,
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: Account.authenticationToken(),
+    },
 };
 
-export const changeConfigHeader = (key, value) => {
-  _config[key] = value;
+export const changeConfigHeader = (key: any, value: any) => {
+    _config[key] = value;
 };
 
 export const exception = {
-  status: false,
-  title: "خطایی رخ داد",
-  message: "خطایی در برقرای ارتباط با سرور پیش آمد",
+    status: false,
+    title: "خطایی رخ داد",
+    message: "خطایی در برقرای ارتباط با سرور پیش آمد",
 };
 
 export const apiCall = Axios.create(_config);
