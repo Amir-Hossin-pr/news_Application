999ab4ea78cef9db76495a747d5978410c573cc1
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-23 18:26:40 +0330

    change redirect middleware

--- News/News.User.Panel/src/router/middleware/guest.ts
+++ News/News.User.Panel/src/router/middleware/guest.ts
@@ -4,7 +4,7 @@ import { Middleware } from "./types";
 export default function (middleware: Middleware) {
     if (middleware.store.getters.auth.isLogin) {
         return middleware.next({
-            name: "account"
+            name: "profile"
         });
     }
     return middleware.next();
