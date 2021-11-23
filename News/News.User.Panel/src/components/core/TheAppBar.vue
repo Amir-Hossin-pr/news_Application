5f03af5380436c11a4feec3f81ef4f22d8f4ce8c
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-23 18:27:22 +0330

    change components

--- News/News.User.Panel/src/components/core/TheAppBar.vue
+++ News/News.User.Panel/src/components/core/TheAppBar.vue
@@ -1,49 +1,47 @@
 <template>
-  <div>
-    <v-app-bar color="deep-purple" dark>
-      <v-app-bar-nav-icon @click="drawer"></v-app-bar-nav-icon>
-      <v-btn @click="back" icon v-if="backward">
-        <v-icon>mdi-backburger</v-icon>
-      </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon :to="{name:'ShopCart'}">
-      <v-icon>mdi-shopping-outline</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <div>
+        <v-app-bar color="deep-purple" dark>
+            <v-app-bar-nav-icon @click="drawer"></v-app-bar-nav-icon>
+            <v-btn @click="back" icon v-if="backward">
+                <v-icon>mdi-backburger</v-icon>
+            </v-btn>
+            <v-toolbar-title>{{ title }}</v-toolbar-title>
+        </v-app-bar>
 
-    <TheNavigation />
-  </div>
+        <TheNavigation />
+    </div>
 </template>
 
 <script>
-import TheNavigation from "./TheNavigation";
-export default {
-  components: { TheNavigation },
-  data: () => ({
-    title: "",
-    backward: false,
-  }),
-  created() {   
-    this.setData();
-  },
-  watch:{
-    $route(){
-      this.setData();
-    }
-  },
-  methods: {
-    drawer() {
-      this.$root.$refs.navigationDrawer.open();
-    },
-    setData() {
-      let meta = this.$route.meta(this.$route);
-      this.title = meta.title;
-      this.backward = meta.hasBackButton;
-    },
-    back() {
-      this.$router.back();
-    },
-  },
-};
+    import TheNavigation from "./TheNavigation.vue";
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        components: { TheNavigation },
+        data: () => ({
+            title: "",
+            backward: false,
+        }),
+        created() {
+            this.setData();
+        },
+        watch: {
+            $route() {
+                this.setData();
+            }
+        },
+        methods: {
+            drawer() {
+                this.$root.$refs.navigationDrawer.open();
+            },
+            setData() {
+                let meta = this.$route.meta(this.$route);
+                this.title = meta.title;
+                this.backward = meta.hasBackButton;
+            },
+            back() {
+                this.$router.back();
+            },
+        },
+    })
 </script>
