5b6458606a620d56eae3ac39f446985f312f34f9
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-27 16:25:38 +0330

    Create AppBottomNavigation.vue

---
+++ News/News.User.Panel/src/components/core/AppBottomNavigation.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="overflow-hidden" v-if="show">
+        <v-bottom-navigation :value="value"
+                             color="primary"
+                             grow
+                             shift>
+            <v-btn v-for="item in items" :key="item.id" :to="item.to">
+                <span>{{item.title}}</span>
+
+                <v-icon>{{item.icon}}</v-icon>
+            </v-btn>
+
+
+        </v-bottom-navigation>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import { bottomItems } from "@/consts/"
+
+    export default Vue.extend({
+        data: () => ({
+            items: bottomItems,
+            value: 0,
+            show: false
+        }),
+        props: {
+            showInMobile: Boolean
+        },
+        created() {
+            this.setShow();
+        },
+        methods: {
+            setShow() {
+                if (!this.showInMobile)
+                    this.show = true;
+                else {
+                    this.show = window.screen.width < 780;
+                }
+            }
+        }
+    })
+
+</script>
