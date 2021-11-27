14ccc0b4e38639663ab06e652072ae5309ae8a7e
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-27 16:26:14 +0330

    Update TheCarousel.vue

--- News/News.User.Panel/src/components/core/TheCarousel.vue
+++ News/News.User.Panel/src/components/core/TheCarousel.vue
@@ -1,74 +1,63 @@
 <template>
-  <div class="carousel-body">
-    <div class="carousel-other">
-      <div class="carousel-other-item">
-        <carousel
-          :items="1"
-          :dots="false"
-          :autoplay="true"
-          :loop="true"
-          :nav="false"
-        >
-          <div class="carousel-other-container" v-for="i in 15" :key="i">
-            <div class="carousel-item">
-              <img
-                src="https://localhost:5001/Images/Products/product_e54b4d4a-a8ea-4878-98ef-ce775fbfa883.png"
-                alt=""
-              />
+    <div class="carousel-body">
+        <div class="carousel-other">
+            <div class="carousel-other-item">
+                <carousel :items="1"
+                          :dots="false"
+                          :autoplay="true"
+                          :loop="true"
+                          :nav="false">
+                    <div class="carousel-other-container" v-for="i in 15" :key="i">
+                        <div class="carousel-item">
+                            <img src="http://localhost:3000/images/news/d8c1220c-a3a5-4abd-9d9d-be1d229d055c1638017578727.jpeg"
+                                 alt="" />
+                        </div>
+                    </div>
+                </carousel>
             </div>
-          </div>
-        </carousel>
-      </div>
-      <div class="carousel-other-item">
-        <carousel
-          :items="1"
-          :dots="false"
-          :autoplay="true"
-          :loop="true"
-          :nav="false"
-        >
-          <div class="carousel-other-container" v-for="i in 15" :key="i">
-            <div class="carousel-item">
-              <img
-                src="https://localhost:5001/Images/Products/product_e54b4d4a-a8ea-4878-98ef-ce775fbfa883.png"
-                alt=""
-              />
+            <div class="carousel-other-item">
+                <carousel :items="1"
+                          :dots="false"
+                          :autoplay="true"
+                          :loop="true"
+                          :nav="false">
+                    <div class="carousel-other-container" v-for="i in 15" :key="i">
+                        <div class="carousel-item">
+                            <img src="http://localhost:3000/images/news/d8c1220c-a3a5-4abd-9d9d-be1d229d055c1638017578727.jpeg"
+                                 alt="" />
+                        </div>
+                    </div>
+                </carousel>
             </div>
-          </div>
-        </carousel>
-      </div>
-    </div>
-    <div class="carousel-main">
-      <carousel
-        :items="1"
-        :dots="false"
-        :autoplay="true"
-        :loop="true"
-        :nav="false"
-      >
-        <div class="carousel-container" v-for="i in 15" :key="i">
-          <div class="carousel-item">
-            <img
-              src="https://localhost:5001/Images/Products/product_e54b4d4a-a8ea-4878-98ef-ce775fbfa883.png"
-              alt=""
-            />
-          </div>
         </div>
-      </carousel>
+        <div class="carousel-main">
+            <carousel :items="1"
+                      :dots="false"
+                      :autoplay="true"
+                      :loop="true"
+                      :nav="false">
+                <div class="carousel-container" v-for="i in 15" :key="i">
+                    <div class="carousel-item">
+                        <img src="http://localhost:3000/images/news/d8c1220c-a3a5-4abd-9d9d-be1d229d055c1638017578727.jpeg"
+                             alt="" />
+                    </div>
+                </div>
+            </carousel>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import carousel from "vue-owl-carousel";
+    import Vue from 'vue'
+    import carousel from "vue-owl-carousel";
 
-export default {
-  components: {
-    carousel,
-  },
-};
+    export default Vue.extend({
+        components: {
+            carousel,
+        },
+    });
 </script>
 
-<style >
-@import "../../assets/style/craousel.css";
+<style>
+    @import "../../assets/style/craousel.css";
 </style>
