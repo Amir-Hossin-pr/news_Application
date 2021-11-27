a2fad615300e82423ae7a536700240ba8e835ecc
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-27 16:25:47 +0330

    Create Index.vue

---
+++ News/News.User.Panel/src/pages/home/Index.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div>
+        <TheCarousel/>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import TheCarousel from "@/components/core/TheCarousel.vue"
+
+    export default Vue.extend({
+        data: () => ({
+
+        }),
+        components: {
+            TheCarousel
+        }
+    })
+</script>
