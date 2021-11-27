472d65ad1c88c04765030f701713830f22070e15
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-27 16:25:50 +0330

    Update account.vue

--- News/News.User.Panel/src/layout/account.vue
+++ News/News.User.Panel/src/layout/account.vue
@@ -3,6 +3,7 @@
         <div class="account-container">
             <router-view />
         </div>
+        <AppBottomNavigation :showInMobile="true" />
         <TheSnackbar></TheSnackbar>
         <TheLoading></TheLoading>
     </v-app>
@@ -13,11 +14,13 @@
 
     import TheLoading from "@/components/core/TheLoading.vue";
     import TheSnackbar from "@/components/core/TheSnackbar.vue";
+    import AppBottomNavigation from "@/components/core/AppBottomNavigation.vue"
 
     export default Vue.extend({
         components: {
             TheLoading,
             TheSnackbar,
+            AppBottomNavigation
         },
     })
 </script>
