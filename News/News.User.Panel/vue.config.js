be4fd5ad0c9643b0a9832e5fdf9c433bfdb6ff53
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-27 19:41:44 +0330

    Update vue.config.js

--- News/News.User.Panel/vue.config.js
+++ News/News.User.Panel/vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    transpileDependencies: [
+        'vuetify'
+    ],
+    pwa: {
+        name: 'News',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+
+        // configure the workbox plugin
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: './src/registerServiceWorker.ts',
+            // ...other Workbox options...
+        }
+    }
 }
