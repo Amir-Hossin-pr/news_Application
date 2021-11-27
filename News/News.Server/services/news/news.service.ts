c627ebe37169b437768c1ffb173412750d327321
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-27 14:54:47 +0330

    Update news.service.ts

--- News/News.Server/services/news/news.service.ts
+++ News/News.Server/services/news/news.service.ts
@@ -2,7 +2,7 @@
 import sequlize from "../../dataBase/context/index"
 import { messages } from "../../consts/index"
 import { PaginationModel } from "../pagination";
-import { saveImage } from "../image.service"
+import { createImageAddress, saveImage } from "../image.service"
 
 export class NewsServices {
     async getClientNews(pagination: PaginationModel) {
@@ -12,15 +12,10 @@ export class NewsServices {
                 limit: pagination.count,
                 offset: (pagination.page * pagination.count),
             });
-            return {
-                status: true,
-                code: 200,
-                title: 'Success',
-                list: news.rows,
-                pages: news.count / pagination.count
-            }
-        } catch {
-            return messages.exception
+            return await this.createNewsListResult(news, pagination.count)
+        }
+        catch (e) {
+            return messages.exception(e.message);
         }
     }
 
@@ -30,17 +25,24 @@ export class NewsServices {
                 limit: pagination.count,
                 offset: (pagination.page * pagination.count),
             });
-
-            return {
-                status: true,
-                code: 200,
-                title: 'Success',
-                list: news.rows,
-                pages: news.count / pagination.count
-            }
+            return await this.createNewsListResult(news, pagination.count)
         }
-        catch {
-            return messages.exception;
+        catch (e) {
+            return messages.exception(e.message);
+        }
+    }
+
+    async createNewsListResult(news, count: number) {
+        news.rows.forEach((news) => {
+            news.get().image = createImageAddress(news.get().image, "news")
+        })
+
+        return {
+            status: true,
+            code: 200,
+            title: 'Success',
+            list: news.rows,
+            pages: news.count / count
         }
     }
 
@@ -48,26 +50,70 @@ export class NewsServices {
         try {
             let image = await saveImage({
                 base64: news.base64,
-                path : "news"
+                path: "news"
             })
 
-            await sequlize.models.News.create({
+            let recentNews = {
                 title: news.title,
                 shortDescription: news.shortDescription,
                 isActive: news.isActive,
                 text: news.text,
                 image: image
-            });
-            return true;
-        } catch {
-            return messages.exception
+            }
+
+            let created = await (await sequlize.models.News.create(recentNews)).get()
+            created.image = createImageAddress(recentNews.image, "news");
+
+            return messages.success({
+                title: "Success",
+                message: "New news was made successfully",
+                result: created
+            })
+        } catch (e) {
+            return messages.exception(e.message)
         }
     }
 
-    async deleteNews(id) {
+    async updateNews(news: NewsModel) {
         try {
-            await sequlize.models.News.drop(id);
-            return true;
+            let findNews = await sequlize.models.News.findOne({ where: { id: news.id } });
+            if (findNews) {
+                let image = (news.base64 != "" && news.base64.indexOf("http") == -1) ?
+                    await saveImage({
+                        base64: news.base64,
+                        path: "news"
+                    }) :
+                    findNews.get().image
+
+                let updated = await findNews.update({
+                    image: image,
+                    title: news.title,
+                    text: news.text,
+                    shortDescription: news.shortDescription,
+                    isActive: news.isActive
+                })
+                updated.get().image = createImageAddress(updated.get().image, "news");
+                return messages.success({
+                    title: "Success",
+                    message: "Update News Successfully",
+                    result: updated
+                })
+            } else
+                return messages.notFound("No news you want to update")
+        }
+        catch (e) {
+            return messages.exception(e.message)
+        }
+    }
+
+    async deleteNews(id: number) {
+        try {
+            await sequlize.models.News.destroy({ where: { id: id } })
+            return messages.success({
+                title: "Success",
+                message: "Success to delete news",
+                result: {}
+            })
         } catch {
             return messages.exception
         }
