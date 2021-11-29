341f67de2fd3744e4c1b24bc17882a2b51311dfc
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-29 18:13:58 +0330

    Update home.vue

--- News/News.User.Panel/src/layout/home.vue
+++ News/News.User.Panel/src/layout/home.vue
@@ -6,6 +6,8 @@
                 <router-view />
             </v-main>
             <AppBottomNavigation :showInMobile="true" />
+            <TheSnackbar/>
+            <TheLoading/>
         </v-app>
     </div>
 </template>
@@ -14,12 +16,16 @@
     import Vue from 'vue'
     import TheAppBar from '@/components/core/TheAppBar.vue'
     import AppBottomNavigation from "@/components/core/AppBottomNavigation.vue"
+    import TheSnackbar from "@/components/core/TheSnackbar.vue"
+    import TheLoading from "@/components/core/TheLoading.vue"
 
     export default Vue.extend({
         name: 'home_layout',
         components: {
             TheAppBar,
-            AppBottomNavigation
+            AppBottomNavigation,
+            TheSnackbar,
+            TheLoading
         },
         data: () => ({
             hasAppBar: true
