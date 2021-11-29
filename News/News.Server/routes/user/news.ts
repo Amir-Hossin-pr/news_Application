f54eeb521f47ed4b0a8f2911bc932b35a0b3af3b
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-11-29 18:12:57 +0330

    Update news.ts

--- News/News.Server/routes/user/news.ts
+++ News/News.Server/routes/user/news.ts
@@ -14,4 +14,22 @@ router.get("/:page/:count", async (req: express.Request, res: express.Response) 
     res.end()
 })
 
+
+router.get("/search", async (req: express.Request, res: express.Response) => {
+    let query: any = req.query;
+    let news = await newsServices.search(query.q, {
+        page: parseInt(query.page),
+        count: parseInt(query.count)
+    })
+    res.json(news);
+    res.end()
+})
+
+router.get("/getNewsItem", async (req: express.Request, res: express.Response) => {
+    let query: any = req.query;
+    let newsItem = await newsServices.getNewsItem(parseInt(query.id))
+    res.json(newsItem);
+    res.end()
+})
+
 export default router;
